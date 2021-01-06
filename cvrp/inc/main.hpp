18cb1e7bf8795e687f0823d7641cd8f30cf3ece6
steags inc <steags.inc@gmail.com>
2021-01-06 14:30:08 +0100

    cvrp light

---
+++ cvrp/inc/main.hpp
@@ -0,0 +1,21 @@
+/**
+* @file main.hpp
+* @author vss2sn
+* @brief Just includes header files.
+*/
+
+#include "genetic_algorithm.hpp"
+#include "greedy.hpp"
+#include "local_search_intra.hpp"
+#include "local_search_inter_intra.hpp"
+#include "tabu_search.hpp"
+#include "simulated_annealing.hpp"
+
+/**
+* @brief Main function
+* @return int 0
+* @details Main function that creates the problem and calls the various solve functions for each of the solution algorithms
+*/
+int main(int argc, char * argv[]);
+
+std::vector<Vehicle> run_instance(PRP prp, std::string type);
