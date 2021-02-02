1a23584f3c64abb980a13e9d1d7dd2ab23ba2454
steags inc <steags.inc@gmail.com>
2021-02-02 01:16:02 +0100

    visu fix

--- Python interface/Visualisation.py
+++ Python interface/Visualisation.py
@@ -1,4 +1,5 @@
 import dash
+import dash_table
 import dash_core_components as dcc
 import dash_cytoscape as cyto
 import dash_html_components as html
@@ -7,7 +8,9 @@ from prp import *
 from PRP1 import *
 from PRP2 import *
 
-app = dash.Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 test = "../PRP_instances/1PDI_Instance"
 
@@ -24,45 +27,70 @@ pathInput = dcc.Input(
 
 maxTimeStep = prp.l
 
+def marks(m):
+
+    marks=dict()
+
+    for i in range(m+1):
+        marks[i]=str(i)
+
+    return marks
+
 timeStep = dcc.Slider(
      id='timeStep',
     min=1,
     max=maxTimeStep,
     step=1,
     value=1,
+    marks=marks(maxTimeStep)
     )
 
-graph = html.Div(id="graph",children=prp.toDash())
+graph = html.Div(id="graph",children=prp.toDash(),
+        style={"-moz-box-shadow":'inset 0 0 10px #000000',
+                "-webkit-box-shadow": 'inset 0 0 10px',
+                "box-shadow": 'inset 0 0 10px',
+                "border-radius":"10px"})
 
-fileButton = html.Button('fileButton', id='fileButton',n_clicks=0)
-prp1Button = html.Button('prp1Button', id='prp1Button',n_clicks=0)
-prp2Button = html.Button('prp2Button', id='prp2Button',n_clicks=0)
+fileButton = html.Button('Load Instance', id='fileButton',n_clicks=0)
+prp1Button = html.Button('Load PRP1', id='prp1Button',n_clicks=0)
+prp2Button = html.Button('Load PRP2', id='prp2Button',n_clicks=0)
 
-app.layout = html.Div([graph,pathInput,fileButton,prp1Button,prp2Button,timeStep])
+interractions = html.Div([pathInput,fileButton,prp1Button,prp2Button,timeStep],style={"text-align":"center","justify-content":"center","align-content":"center"})
+
+table = [{"QuantityProduced":1,"Inventory":2},{"QuantityProduced":1,"Inventory":2}]
+
+prodT = dash_table.DataTable(
+        id='prodTable',
+        columns=[{"name": "Quantity produced", "id": "QuantityProduced"},{"name": "Inventory", "id": "Inventory"}],
+        data=table,
+)
+
+app.layout = html.Div([html.H1(children='Visualisation',style={"text-align":"center","font-family":"sans-serif"}),graph,interractions,prodT])
 
 @app.callback(
     dash.dependencies.Output('graph','children'),
+    dash.dependencies.Output('prodTable','data'),
     [dash.dependencies.Input('fileButton','n_clicks')],
     [dash.dependencies.Input('prp1Button','n_clicks')],
     [dash.dependencies.Input('prp2Button','n_clicks')],
     [dash.dependencies.Input('timeStep', 'value')],
     [dash.dependencies.State('Path', 'value')])
 def update_output(nc1,nc2,nc3,t,path):
-    global pathInst
+    global pathInst,prp,table
     changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
     pathInst=path
-    global prp
     if 'fileButton' in changed_id:
         prp=PRP(path)
         maxTimeStep = prp.l
     elif 'prp1Button' in changed_id:
         prp=PRP1Sol(path)
+        table=prp.prodTable()
         maxTimeStep = prp.prp.l
     elif 'prp2Button' in changed_id:
         prp=PRP2Sol(path)
         maxTimeStep = prp.prp.l
     if path != None:
-        return prp.toDash(t)
+        return prp.toDash(t),table
 
 if __name__ == '__main__':
     app.run_server(debug=True)
